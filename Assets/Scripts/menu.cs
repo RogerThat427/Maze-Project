a6c20d52a19dddf02483367c9a5a0ed2cf279560
RIGHTRYETHEBREAD <30728817+RIGHTRYETHEBREAD@users.noreply.github.com>
2017-09-05 15:21:28 -0400

    main menu script

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void Quit()
+    {
+        print("Quitting");
+        Application.Quit();
+    }
+}
